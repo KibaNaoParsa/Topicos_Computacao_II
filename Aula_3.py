512dd7cb7069d91ff893c1717036baf93f491c31
Elyas Correa Nogueira <elyasnog2@hotmail.com>
2020-03-12 15:35:03 -0300

    Update Aula_3.py

--- Aula_3.py
+++ Aula_3.py
@@ -1,1 +1,83 @@
-
+# coding: utf-8
+msg = 'ok pessoal :-)'
+len(msg)
+type(msg)
+print(msg)
+print(msg[1])
+print(msg[-1])
+msg[1]
+msg = "olg GALERA do BEM"
+xy = msg.title()
+print(msg)
+print(xy)
+xy = msg.upper()
+print(msg)
+print(xy)
+xy = msg.lower()
+print(msg)
+print(xy)
+xy = msg.swapcase()
+print(msg)
+print(xy)
+xy = msg.replace('ALE', 'AR')
+print(msg)
+print(xy)
+xy = msg.capitalize()
+print(msg)
+print(xy)
+print(msg.isupper())
+print(msg.is_upper())
+print(msg.isupper())
+print(msg.islower())
+codigo_01 = "874"
+codigo_02 = "BX874"
+codigo_03 = "BX-874"
+codigo_01.isdigit()
+codigo_02.isdigit()
+codigo_03.isdigit()
+codigo_01.isalpha()
+codigo_02.isalpha()
+codigo_03.isalpha()
+codigo_04 = "BXY"
+codigo_04.isalpha()
+x =codigo_04.isalpha()
+type(x)
+x = print(codigo_04.isalpha())
+type(x)
+codigo_01.isalnum()
+codigo_02.isalnum()
+codigo_03.isalnum()
+codigo_04.isalnum()
+a = (5, 6)
+b = [4, 5, 6]
+b.count()
+b.count
+b.count(5)
+c = complex(3, 4)
+print(c)
+msg = "ola GALERA do BEM"
+xy = [7, 3, 'oi', 'casa', 4.7]
+msg[4:9]
+msg[4:10]
+msg[4:-2]
+msg[2[0]:2[1]]
+msg[:]
+msg[2:]
+msg[:-1]
+msg[7:]
+msg[:]
+msg[2:14:2]
+msg[9:3]
+msg[9:3:-1]
+msg[::-1]
+mg = "roma"
+mg[:-1]
+mg[:-1:-1]
+mg[-1]
+mg[1::-1]
+mg[1:]
+mg[1:][:-1]
+mg[1:][::-1]
+mg[:-2:]
+mg[::-2]
+mg[1:-1]
